f2359c82b3fa227f3c19cb680ae1da54e88c8947
erickkurniawan <erick.kurniawan@gmail.com>
2023-10-03 15:40:32 +0700

    crud untuk sql server

--- Services/SqlServerRestaurantData.cs
+++ Services/SqlServerRestaurantData.cs
@@ -18,17 +18,73 @@ namespace MyASPWeb.Services
         }
         public Restaurant Add(Restaurant newT)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                string strSql = @"insert into Restaurants (Name) 
+                                   values (@Name);select scope_identity()";
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+                cmd.Parameters.AddWithValue("@Name", newT.Name);
+                try
+                {
+                    conn.Open();
+                    var newId = Convert.ToInt32(cmd.ExecuteScalar());
+                    newT.Id = newId;
+                    return newT;
+                }
+                catch (SqlException sqlEx)
+                {
+                    throw new Exception(sqlEx.Message);
+                }
+            }
         }
 
         public Restaurant Delete(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                string strSql = @"delete from Restaurants where Id=@Id";
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                try
+                {
+                    conn.Open();
+                    int result = cmd.ExecuteNonQuery();
+                    if (result != 1)
+                    {
+                        throw new Exception("Data tidak ditemukan!");
+                    }
+
+                    return new Restaurant { Id = id };
+                }
+                catch (SqlException sqlEx)
+                {
+                    throw new Exception(sqlEx.Message);
+                }
+            }
         }
 
         public Restaurant Get(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                string strSql = @"select * from Restaurants where Id=@Id";
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    dr.Read();
+                    Restaurant restaurant = new Restaurant
+                    {
+                        Id = Convert.ToInt32(dr["Id"]),
+                        Name = dr["Name"].ToString()
+                    };
+                    return restaurant;
+                }
+                return null;
+            }
         }
 
         public IEnumerable<Restaurant> GetAll()
@@ -63,7 +119,24 @@ namespace MyASPWeb.Services
 
         public Restaurant Update(Restaurant updatedT)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                string strSql = @"update Restaurants set Name=@Name where Id=@Id";
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+                cmd.Parameters.AddWithValue("@Name", updatedT.Name);
+                cmd.Parameters.AddWithValue("@Id", updatedT.Id);
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    return updatedT;
+                }
+                catch (SqlException sqlEx)
+                {
+                    throw new Exception(sqlEx.Message);
+                }
+            }
         }
     }
 }
