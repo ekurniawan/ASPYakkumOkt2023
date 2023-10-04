2e3b34c0c5f80fe0088eaff080ab68b0b9897cc4
erickkurniawan <erick.kurniawan@gmail.com>
2023-10-04 14:20:55 +0700

    menambahkan registrasi

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,23 +3,59 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MyASPWeb.ViewModels;
 
 namespace MyASPWeb.Controllers
 {
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(ILogger<AccountController> logger,
+        UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             _logger = logger;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Copy data from RegisterViewModel to IdentityUser
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+                // Store user data in AspNetUsers database table
+                var result = await _userManager.CreateAsync(user, model.Password);
+                // If user is successfully created, sign-in the user using
+                // SignInManager and redirect to index action of HomeController if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                // If there are any errors, add them to the ModelState object
+                // which will be displayed by the validation summary tag helper 
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(model);
         }
 
         public IActionResult Index()
